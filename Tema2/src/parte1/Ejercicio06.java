8f662826a97da973a96e9c570792f80225f6cd90
ivan.zamora <ivan.zamora@106-30.info2>
2024-10-07 12:07:04 +0200

    6 acabado

--- Tema2/src/parte1/Ejercicio06.java
+++ Tema2/src/parte1/Ejercicio06.java
@@ -6,38 +6,72 @@ public class Ejercicio06 {
 
 	public static void main(String[] args) {
 		
+		// Creamos el scanner
 		Scanner sc = new Scanner(System.in);
 		
-		int a;
+		// Creamos la variable donde guardaremos el valor de "a"
+		int valorA;
 		
-		int b;
+		// Creamos la variable donde guardaremos el valor de "b"
+		int valorB;
 		
-		int c;
+		// Creamos la variable donde guardaremos el valor de "c"
+		int valorC;
 		
+		// Creamos la variable donde guardaremos el resultado tras seguir la suma en la ecuación
 		double resultadoPositivo;
 		
+		// Creamos la variable donde guardaremos el resultado tras seguir la resta en la ecuación
 		double resultadoNegativo;
 		
-		System.out.println("Introduzca el valor de a para calcular x");
+		// Le pedimos al usuario que introduzca el valor de "a"
+		System.out.println("Introduzca el valor de 'a' para calcular x");
 		
-		a = sc.nextInt();
+		// Introducimos el valor recibido en la variable valorA
+		valorA = sc.nextInt();
 		
-		System.out.println("Introduzca el valor de b");
+		// Le pedimos al usuario que introduzca el valor de "b"
+		System.out.println("Introduzca el valor de 'b'");
 		
-		b = sc.nextInt();
+		// Introducimos el valor recibido en la variable valorB
+		valorB = sc.nextInt();
 		
-		System.out.println("Introduzca el valor de c");
+		// Le pedimos al usuario que introduzca el valor de "c"
+		System.out.println("Introduzca el valor de 'c'");
 		
-		c = sc.nextInt();
+		// Introducimos el valor recibido en la variable valorC
+		valorC = sc.nextInt();
 		
-		resultadoPositivo = ((-b + Math.sqrt(b * b - 4 * a * c )) / (2 * a));
+		// Verificamos si la parte que se encuentra en la raíz cuadrada es menor a cero, es decir, es negativo
+		if ((valorB * valorB - 4 * valorA * valorC) < 0) {
+			
+			// En caso de ser así, no se podrá completar la ecuación por lo que se lo notificamos al usuario
+			System.out.println("Se ha encontrado un error durante la ecuación: No se puede hacer la raíz cuadrada de un número negativo");
+			
+		// Si se puede continuar, verificamos ahora si "a" es igual a 0, ya que en ese caso ni si quiera es una ecuación de segundo grado, además en la ecuación se tendrá que dividir entre 0, cosa que no es posible
+		} else if (valorA == 0) {
+			
+			// Se lo notificamos al usuario, justificando que no sería una ecuación de segundo grado
+			System.out.println("Se ha encontrado un error durante la ecuación: 'a' no puede ser 0, ya que si es el caso no es una ecuación de segundo grado, además de que no se puede dividir entre 0");
+			
+		// En caso de no haber ningun error, podemos continuar con la ecuación
+		} else {
+			
+			// Calculamos la ecuación siguiendo la suma y lo introducimos en la variable resultadoPositivo
+			resultadoPositivo = ((-valorB + Math.sqrt(valorB * valorB - 4 * valorA * valorC )) / (2 * valorA));
+			
+			// Calculamos la ecuación siguiendo la resta y lo introducimos en la variable resultadoNegativo	
+			resultadoNegativo = ((-valorB - Math.sqrt(valorB * valorB - 4 * valorA * valorC )) / (2 * valorA));
+			
+			// Le mostramos al usuario el resultado obtenido siguiendo la suma como x1
+			System.out.println("x1 = " + resultadoPositivo);
+			
+			// Le mostramos al usuario el resultado obtenido siguiendo la resta como x2
+			System.out.println("x2 = " + resultadoNegativo);
+			
+		}
 		
-		resultadoNegativo = ((-b - Math.sqrt(b * b - 4 * a * c )) / (2 * a));
-		
-		System.out.println(resultadoPositivo);
-		
-		System.out.println(resultadoNegativo);
-		
+		// Cerramos el scanner
 		sc.close();
 
 	}
