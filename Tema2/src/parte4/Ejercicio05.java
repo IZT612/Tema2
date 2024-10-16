7bcddbe8511dbd18cfd5bcb716017a502fc50982
ivan.zamora <ivan.zamora@106-30.info2>
2024-10-16 08:42:41 +0200

    parte 4, whiles, terminados

--- Tema2/src/parte4/Ejercicio05.java
+++ Tema2/src/parte4/Ejercicio05.java
@@ -6,47 +6,82 @@ public class Ejercicio05 {
 
 	public static void main(String[] args) {
 		
+		// Creamos el scanner
 		Scanner sc = new Scanner(System.in);
 		
+		// Creamos la variable "edad" donde guardaremos la edad introducida por el usuario
 		int edad;
 		
+		// Creamos la variable "suma" donde guardaremos la suma de todas las edades
 		int suma = 0;
 		
+		// Creamos la variable "contadorTotal" donde guardaremos la cantidad de alumnos que se han introducido
 		double contadorTotal = 0;
 		
+		// Creamos la variable "contadorMayores" donde guardaremos la cantidad de alumnos que son mayores de edad
 		double contadorMayores = 0;
 		
+		// Le pedimos al usuario que introduzca la edad del alumno.
 		System.out.println("Introduzca la edad del alumno. Introduzca un número negativo para finalizar el programa");
 		
+		// Introducimos el valor recibido en la variable "edad"
 		edad = sc.nextInt();
 		
+		// Creamos un bucle mientras que la edad sea mayor a 0
 		while (edad > 0) {
 			
+			// Sumamos 1 al contador total
 			contadorTotal++;
 			
+			// Sumamos la edad a la variable "suma"
 			suma += edad;
 			
+			// Mediante un operador ternario comprobamos si la edad es mayor o igual a 18 sumamos 1 al contador de mayores de edad, si no, no lo modificamos
 			contadorMayores = edad >= 18 ? contadorMayores + 1 : contadorMayores;
 			
+			// Le pedimos al usuario que introduzca nuevamente la edad
 			System.out.println("Introduzca la edad del alumno. Introduzca un número negativo para finalizar el programa");
 			
+			// Introducimos el valor recibido en la variable "edad"
 			edad = sc.nextInt();
 			
 		}
 		
+		// Si no se han introducido edades positivas entramos en el condicional
 		if (contadorTotal == 0) {
 			
+			// Le informamos al usuario de que no ha introducido ningun alumno
 			System.out.println("No se ha introducido ningún alumno");
 			
+		// Si es correcto, le mostramos todo lo que pide
 		} else {
 			
+			// Mostramos la suma de todas las edades
 			System.out.println("La suma de todas las edades es " + suma);
+			
+			// Mostramos la media de todas las edades
 			System.out.println("La media de las edades es " + (suma / contadorTotal));
+			
+			// Mostramos la cantidad total de alumnos
 			System.out.println("La cantidad de alumnos totales es de: " + contadorTotal);
-			System.out.println("La cantidad de alumnos mayores de edad es de: " + contadorMayores);
+			
+			// Si no se han introducido mayores de edad entramos en el condicional
+			 if (contadorMayores == 0) {
+				 
+				// Le mostramos al usuario que no ha introducido alumnos mayores de edad
+				System.out.println("No se han introducido alumnos mayores de edad");
+				 
+			// Si no es el caso procedemos
+			 } else {
+				 
+				 // Y le mostramos al usuario la cantidad de alumnos mayores de edad
+				System.out.println("La cantidad de alumnos mayores de edad es de: " + contadorMayores);
+				 
+			 }
 			
 		}
 		
+		// Cerramos el scanner
 		sc.close();
 			
 	}
--- Tema2/src/parte4/Ejercicio06.java
+++ Tema2/src/parte4/Ejercicio06.java
@@ -1,11 +1,62 @@
 package parte4;
 
-
+import java.util.Scanner;
 
 public class Ejercicio06 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Creamos el scanner
+		Scanner sc = new Scanner(System.in);
+		
+		// Creamos la variable "altura" donde guardaremos la altura introducida por el usuario
+		int altura;
+		
+		// Creamos la variable "recordAltura" donde guardaremos la altura más alta introducida por el usuario
+		int recordAltura = 0;
+		
+		// Le pedimos al usuario que introduzca la altura del árbol en centímetros
+		System.out.println("Introduzca la altura del árbol en centímetros");
+		
+		// Introducimos el valor recibido en la variable "altura"
+		altura = sc.nextInt();
+		
+		// Creamos un bucle mientras que la altura sea mayor a 0
+		while (altura > 0) {
+			
+			// Si la altura es mas grande al record entramos en el condicional
+			if (altura > recordAltura) {
+				
+				// Y guardamos el nuevo record de altura
+				recordAltura = altura;
+				
+			// Si no es el caso, cerramos sin modificar nada
+			}
+			
+			// Le pedimos al usuario que introduzca la altura de otro árbol
+			System.out.println("Introduzca la altura de otro árbol en centímetros, introduzca uno negativo para finalizar");
+			
+			// Introducimos el valor recibido en la variable "altura"
+			altura = sc.nextInt();
+			
+		}
+		
+		// Si el record de altura es 0 entramos en el condicional
+		if (recordAltura == 0) {
+			
+			// Y le informamos al usuario que no ha introducido ningúna altura válida
+			System.out.println("No se ha introducido la altura de ningún árbol");
+			
+		// Si no es el caso, procedemos
+		} else {
+			
+			// Y le mostramos al usuario el record de altura
+			System.out.println("La altura del árbol más grande es de " + recordAltura + " centímetros");
+			
+		}
+		
+		// Cerramos el scanner
+		sc.close();
 
 	}
 
