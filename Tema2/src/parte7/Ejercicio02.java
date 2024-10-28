3a0d0b5470f1ae3d3673936069f619c01c2e64bd
IZT612 <ivan.zamora@iesnervion.es>
2024-10-28 12:45:27 +0100

    Hasta el 3

--- Tema2/src/parte7/Ejercicio02.java
+++ Tema2/src/parte7/Ejercicio02.java
@@ -6,43 +6,60 @@ public class Ejercicio02 {
 
 	public static void main(String[] args) {
 		
+		// Creamos el scanner
 		Scanner sc = new Scanner(System.in);
 		
+		// Creamos la variable "numero" donde guardaremos el numero introducido por el usuario
 		int numero;
 		
-		int sumaPrimos = 0;
+		// Creamos la variable "cantidadPrimos" donde guardaremos la cantidad de primos que hay del 1 al numero introducido por el usuario
+		int cantidadPrimos = 0;
 		
+		// creamos la variable booleana "primo" donde verificaremos si el numero es primo o no
 		boolean primo;
 		
+		// Le pedimos al usuario que introduzca un numero
 		System.out.println("Introduzca un número");
 		
+		// Introducimos el valor recibido en la variable "numero"
 		numero = sc.nextInt();
 		
-		for (int numerosPrimos = 2 ; numerosPrimos < numero ; numerosPrimos++) {
+		// Creamos un bucle for donde comenzamos a contar desde el 2, y vamos incrementandolo, y se repetira mientras el numero a verificar sea menor o igual al numero del usuario
+		for (int numerosPrimos = 2 ; numerosPrimos <= numero ; numerosPrimos++) {
 			
+			// Deducimos que todos los numeros son primos hasta demostrar lo contrario
 			primo = true;
 			
+			// Con otro bucle for, comenzamos a contar desde el 2, y vamos incrementandolo, y se repetira mientras que sea primo y el contador no alcance el número a verificar
 			for (int i = 2 ; primo && i != numerosPrimos ; i++ ) {
 				
-				if (numerosPrimos % i == 0 && primo) {
+				// Si el número se puede dividir entre el contador entramos al condicional
+				if (numerosPrimos % i == 0) {
 					
+					// Por lo que ya no es primo
 					primo = false;
 					
 				}
 				
 			}
 			
+			// Al acabar el bucle, si sigue siendo primo, entramos al condicional
 			if (primo) {
 				
+				// Le mostramos al usuario el número
 				System.out.println(numerosPrimos);
-				sumaPrimos++;
+				
+				// Incrementamos en 1 la cantidad de numeros primos
+				cantidadPrimos++;
 			}
 					
 		}
 		
-		System.out.println("Son " + sumaPrimos + " números primos en total");
+		// Le mostramos al usuario la cantidad de numeros primos final
+		System.out.println("Son " + cantidadPrimos + " números primos en total");
 
 		
+		// Cerramos el scanner
 		sc.close();
 
 	}
---
+++ Tema2/src/parte7/Ejercicio03.java
@@ -0,0 +1,80 @@
+package parte7;
+
+import java.util.Scanner;
+
+public class Ejercicio03 {
+
+	public static void main(String[] args) {
+
+		// Creamos el scanner
+		Scanner sc = new Scanner(System.in);
+		
+		// Creamos las variables "numeroA" y "numeroB" donde guardamos los numeros introducidos por el usuario
+		int numeroA;
+		int numeroB;
+		
+		// Creamos la variable "menor" donde guardaremos el número más pequeño
+		int menor;
+		
+		// Creamos la variable "mcd" donde guardaremos el máximo comun divisor
+		int mcd = 0;
+		
+		// Creamos la variable booleana "hayMcd" donde guardaremos si se ha encontrado máximo comun divisor
+		boolean hayMcd = false;
+		
+		// Le pedimos al usuario el primer numero
+		System.out.println("Introduzca el primer número");
+		
+		// Lo guardamos en la variable "numeroA"
+		numeroA = sc.nextInt();
+		
+		// Le pedimos al usuario el primer numero
+		System.out.println("Introduzca ahora el segundo número");
+		
+		// Lo guardamos en la variable "numeroB"
+		numeroB = sc.nextInt();
+		
+		// Si el número A es menor al número B lo ponemos como menor
+		if (numeroA < numeroB) {
+			
+			menor = numeroA;
+			
+		// Si es al contrario, el B será menor
+		} else if (numeroB < numeroA) {
+			
+			menor = numeroB;
+			
+		// Si no es el caso son iguales, solo hay un posible máximo comun divisor
+		} else {
+			
+			menor = 0;
+			mcd = numeroA;
+			hayMcd = true;
+			
+		}
+		
+		// Creamos un bucle for, con un contador que empieza como el menor de los 2, y se repite hasta que haya maximo comun divisor, tambien vamos restando el contador -1 por cada iteracion
+		for (int i = menor ; !hayMcd ; i--) {
+			
+			// Comprobamos si puede dividir a ambos números y entramos al condicional si es el caso
+			if (numeroA % i == 0 && numeroB % i == 0) {
+				
+				// El máximo comun divisor será el contador
+				mcd = i;
+				
+				// Hemos encontrado maximo comun divisor asi que ponemos "hayMcd" como true
+				hayMcd = true;
+				
+			}
+			
+		}
+		
+		// Le mostramos al usuario el máximo comun divisor
+		System.out.println("El máximo común divisor es: " + mcd);
+		
+		// Cerramos el scanner
+		sc.close();
+		
+	}
+
+}
